039875f1426a51fa92e898e4013ab593a33ced92
Malaya2184 <malayakumarswain8@gmail.com>
2021-09-05 01:24:12 +0530

    gettig started with angular cli

---
+++ cwh-todo-list/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'cwh-todo-list';
+  constructor(){
+    setTimeout(() => {
+      this.title = "this is spider";
+    },5000)
+  }
+}
